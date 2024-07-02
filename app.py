9497097827e574fa0c8efe721e524a6f3d1366b6
tyuukau <bachocnhi2002@gmail.com>
2024-07-02 22:52:37 +0700

    [app] ~ update demo

--- app.py
+++ app.py
@@ -1,44 +1,13 @@
 import gradio as gr
-import cv2
-import tempfile
 from ultralytics import YOLOv10
 
 
-def yolov10_inference(image, video, image_size, conf_threshold):
-    model = YOLOv10("./weights/best.pt")
+def yolov10_inference(image, image_size, conf_threshold):
+    model = YOLOv10("/content/best.pt")
     if image:
         results = model.predict(source=image, imgsz=image_size, conf=conf_threshold)
         annotated_image = results[0].plot()
-        return annotated_image[:, :, ::-1], None
-    else:
-        video_path = tempfile.mktemp(suffix=".webm")
-        with open(video_path, "wb") as f:
-            with open(video, "rb") as g:
-                f.write(g.read())
-
-        cap = cv2.VideoCapture(video_path)
-        fps = cap.get(cv2.CAP_PROP_FPS)
-        frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-        output_video_path = tempfile.mktemp(suffix=".webm")
-        out = cv2.VideoWriter(
-            output_video_path, cv2.VideoWriter_fourcc(*"vp80"), fps, (frame_width, frame_height)
-        )
-
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
-
-            results = model.predict(source=frame, imgsz=image_size, conf=conf_threshold)
-            annotated_frame = results[0].plot()
-            out.write(annotated_frame)
-
-        cap.release()
-        out.release()
-
-        return None, output_video_path
+        return annotated_image[:, :, ::-1]
 
 
 def app():
@@ -46,9 +15,8 @@ def app():
         with gr.Row():
             with gr.Column():
                 image = gr.Image(type="pil", label="Image", visible=True)
-                video = gr.Video(label="Video", visible=False)
                 input_type = gr.Radio(
-                    choices=["Image", "Video"],
+                    choices=["Image"],
                     value="Image",
                     label="Input Type",
                 )
@@ -70,40 +38,30 @@ def app():
 
             with gr.Column():
                 output_image = gr.Image(type="numpy", label="Annotated Image", visible=True)
-                output_video = gr.Video(label="Annotated Video", visible=False)
 
         def update_visibility(input_type):
             image = (
                 gr.update(visible=True) if input_type == "Image" else gr.update(visible=False)
             )
-            video = (
-                gr.update(visible=False) if input_type == "Image" else gr.update(visible=True)
-            )
             output_image = (
                 gr.update(visible=True) if input_type == "Image" else gr.update(visible=False)
             )
-            output_video = (
-                gr.update(visible=False) if input_type == "Image" else gr.update(visible=True)
-            )
 
-            return image, video, output_image, output_video
+            return image, output_image
 
         input_type.change(
             fn=update_visibility,
             inputs=[input_type],
-            outputs=[image, video, output_image, output_video],
+            outputs=[image, output_image],
         )
 
-        def run_inference(image, video, image_size, conf_threshold, input_type):
-            if input_type == "Image":
-                return yolov10_inference(image, None, image_size, conf_threshold)
-            else:
-                return yolov10_inference(None, video, image_size, conf_threshold)
+        def run_inference(image, image_size, conf_threshold):
+            return yolov10_inference(image, image_size, conf_threshold)
 
         yolov10_infer.click(
             fn=run_inference,
-            inputs=[image, video, image_size, conf_threshold, input_type],
-            outputs=[output_image, output_video],
+            inputs=[image, image_size, conf_threshold],
+            outputs=[output_image],
         )
 
 
